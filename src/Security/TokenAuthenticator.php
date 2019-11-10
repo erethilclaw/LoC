6296e3a577b32c89c9d472f515d0999cb7cc1ae8
Ernest Riccetto <ernest.riccetto@gmail.com>
2019-11-10 16:06:47 +0100

    api now returns user id when succses login

--- src/Security/TokenAuthenticator.php
+++ src/Security/TokenAuthenticator.php
@@ -13,21 +13,20 @@ class TokenAuthenticator extends JWTTokenAuthenticator
 	/**
 	 * @param PreAuthenticationJWTUserToken $preAuthToken
 	 * @param UserProviderInterface $userProvider
+	 * @return null|\Symfony\Component\Security\Core\User\UserInterface|void
 	 */
-	public function getUser( $preAuthToken, UserProviderInterface $userProvider ) {
-		/**
-		 * @var User $user
-		 */
+	public function getUser($preAuthToken, UserProviderInterface $userProvider)
+	{
+		/** @var User $user */
 		$user = parent::getUser(
-			$preAuthToken, $userProvider
+			$preAuthToken,
+			$userProvider
 		);
-
-		if ( $user->getPasswordChangeDate() &&
-		     $preAuthToken->getPayload()['iat'] < $user->getPasswordChangeDate())
-		{
+		if ($user->getPasswordChangeDate() &&
+		    $preAuthToken->getPayload()['iat'] < $user->getPasswordChangeDate()
+		) {
 			throw new ExpiredTokenException();
 		}
-
 		return $user;
 	}
 }
